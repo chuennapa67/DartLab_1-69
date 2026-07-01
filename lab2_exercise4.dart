1a7608f4166d048933ccc2dd1d84c0fd7b2dd6d1
chuennapa67 <chuennapap67@nu.ac.th>
2026-07-01 11:51:53 +0700

    add exercise

--- lab2_exercise4.dart
+++ lab2_exercise4.dart
@@ -3,11 +3,11 @@ void main(){
   String city = 'Nakhon Sawan';
 
   int length = middleName?.length ?? 0;
-  print('Length middlename : $length');
+  print('Length middleName : $length');
 
   print('Length city :${city.length}');
 
-  print('ก่อนกำหนดค่าmiddlename : $middleName');
+  print('ก่อนกำหนดค่าmiddleName : $middleName');
   middleName ??= 'N/A';
-  print('หลังกำหนดค่าmiddlename : $middleName');
+  print('หลังกำหนดค่าmiddleName : $middleName');
 }
